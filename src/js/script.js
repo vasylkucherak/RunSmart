6e19685a33d721946021584e6403ce33edc6f8a9
vasylkucherak <vasil.kucherak@gmail.com>
2022-01-27 22:50:35 +0200

    v.0.4.0 (component carousel adapted for all devices)

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>'
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
 });
