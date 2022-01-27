da07d13a51088fcde31887fcea62f3eb6ae25732
vasylkucherak <vasil.kucherak@gmail.com>
2022-01-27 18:33:30 +0200

    v.0.3.5 (created a carousel component)

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>'
+    });
+});
